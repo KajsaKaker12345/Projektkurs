0d27fc6514d723b5a21bc0e347bd21fc6526078d
KajsaKaker12345 <kk223ic@student.lnu.se>
2026-05-06 17:48:12 +0200

    fix filter plats

--- filter.js
+++ filter.js
@@ -1,9 +1,20 @@
+/*import { createCards } from "./renderCard.js";
 const response = await fetch("../data.json");
 const data = await response.json();
 const placeInput = document.getElementById("place");
-const form = document.getElementById("field");
-const cardContainer = document.getElementById("card-container");
+const form = document.getElementById("form");
 
-function renderList(list) {
 
-}
+form.addEventListener("change", filterList);
+
+function filterList() {
+    const placeValue = placeInput.value;
+    let filtered = data;
+
+    if (placeValue !== "alla") {
+        filtered = data.filter(d => d.in === placeValue);
+    }
+
+    createCards(filtered);
+    
+}*/
--- main.js
+++ main.js
@@ -4,6 +4,24 @@ const response = await fetch("dates.json");
 const data = await response.json();
 createCards(data);
 
+const placeInput = document.getElementById("place");
+const form = document.getElementById("form");
+
+
+form.addEventListener("change", filterList);
+
+function filterList() {
+    const placeValue = placeInput.value;
+    let filtered = data;
+
+    if (placeValue !== "alla") {
+        filtered = data.filter(d => d.in === placeValue);
+    }
+
+    createCards(filtered);
+    
+}
+
 const btn = document.getElementById("menu-icon");
 const nav = document.getElementById("hidden-nav");
 
