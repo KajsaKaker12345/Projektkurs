b55ac4b1b673e41a4bc0fba9fdf90b21d93ddee3
KajsaKaker12345 <kk223ic@student.lnu.se>
2026-05-11 15:26:19 +0200

    edit img slumpKlöver

--- slumpa/slumpa.js
+++ slumpa/slumpa.js
@@ -34,7 +34,7 @@ function randomizeDates(data) {
     <div class="card-inner">
 
     <div class="card-header">
-    <img src="../image/HjärtaFyllt.svg" alt="">
+    <img src="../image/slumpKlöver.svg" alt="">
     </div>
 
     <div class="card-front">
