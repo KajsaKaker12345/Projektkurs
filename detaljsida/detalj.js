7e884d24302e73601c5de2a5fca4529944fc709e
Nikitabjorknert <nb222tx@student.lnu.se>
2026-05-18 21:46:49 +0200

    fontstyle

--- detaljsida/detalj.js
+++ detaljsida/detalj.js
@@ -29,8 +29,10 @@ async function createDetails(category) {
     }
 
     headerContainer.innerHTML = `<a href="../index.html"><img src="../image/pil.svg" alt="tillbaka pil"></a><h1>${category.name}</h1><img src="../image/spara.svg" alt="image/spara ikon" id="save">`;
+    
     detailsContainer.innerHTML = `
         <img src="../${category.image}" alt="${category.name}">
+        <img src="../${category.rating}" alt="rating" class="stars">
         <p>${category.description}</p>
     `; 
 
