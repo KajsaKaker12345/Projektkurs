a4920e45685b93c862cef5dbb8042feaab9e2e89
Linnea <lk224ac@student.lnu.se>
2026-05-19 20:52:22 +0200

    byt kort

--- slumpa/slumpa.js
+++ slumpa/slumpa.js
@@ -34,7 +34,7 @@ function randomizeDates(data) {
     <div class="card-inner">
 
     <div class="card-header">
-    <img id="klöver-kort" src="../image/slumpKlöver.svg" alt="">
+    <img id="klöver-kort" src="../image/Slumpande.svg" alt="">
     </div>
 
     <div class="card-front">
