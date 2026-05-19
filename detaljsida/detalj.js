3485913195fe08fcf969ca2ff81b249f348f0de3
Nikitabjorknert <nb222tx@student.lnu.se>
2026-05-19 13:22:38 +0200

    Detaljsida buttons

--- detaljsida/detalj.js
+++ detaljsida/detalj.js
@@ -28,7 +28,7 @@ async function createDetails(category) {
         return;
     }
 
-    headerContainer.innerHTML = `<a href="../index.html"><img src="../image/pil.svg" alt="tillbaka pil"></a><h1>${category.name}</h1><img src="../image/spara.svg" alt="image/spara ikon" id="save">`;
+    headerContainer.innerHTML = `<a href="../index.html"><img src="../image/pil.svg" alt="tillbaka pil"></a><h1>${category.name.toUpperCase()}</h1><img src="../image/spara.svg" alt="image/spara ikon" id="save">`;
     
     detailsContainer.innerHTML = `
         <img src="../${category.image}" alt="${category.name}">
