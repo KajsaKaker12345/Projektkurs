a732dbc2c7b936ceceed8494ea62de7c97380c93
Linnea <lk224ac@student.lnu.se>
2026-05-19 20:08:29 +0200

    add slump funktion

--- slumpa/slumpa.js
+++ slumpa/slumpa.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById("menu-icon");
 const nav = document.getElementById("hidden-nav");
+const startGIF = document.getElementById("startaGIF");
+const GIF = document.getElementById("gifen");
+const stilla = document.getElementById("stillBild");
 
 btn.addEventListener("click", () => {
     nav.classList.toggle("active");
@@ -9,17 +12,14 @@ const response = await fetch("../dates.json");
 const data = await response.json();
 
 const cardContainer = document.getElementById("card-container");
-const newCard = document.getElementById("new-btn");
+
 randomizeDates(data);
-
-newCard.addEventListener("click", () => randomizeDates(data));
-
 function randomizeDates(data) {
     cardContainer.innerHTML = "";
 
     const randomDates = [];
 
-     for (let i = 0; i < 6; i++) {
+     for (let i = 0; i < 3; i++) {
     const randomIndex = Math.floor(Math.random() * data.length);
     randomDates.push(data[randomIndex]);
      }
@@ -50,3 +50,29 @@ function randomizeDates(data) {
     
 
 }
+
+
+startGIF.addEventListener("click", ()=>{
+
+    GIF.classList.toggle("active");
+    stilla.classList.toggle("active");
+
+    cardContainer.innerHTML ="";
+
+
+
+    setTimeout(() => {
+        GIF.classList.remove("active");
+        stilla.style.display = "none";
+
+        randomizeDates(data);
+
+        const allaKort = document.querySelectorAll(".slump-dejt");
+
+        allaKort.forEach(card => {
+            card.classList.toggle("active");
+        });
+        
+    }, 5000);
+
+}); 
