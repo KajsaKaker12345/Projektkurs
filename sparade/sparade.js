b26b69a6850d788d0a066fe916ffd39cde11b9f7
Linnea <lk224ac@student.lnu.se>
2026-05-21 15:02:35 +0200

    add kommentar i sparde dejter

--- sparade/sparade.js
+++ sparade/sparade.js
@@ -6,7 +6,15 @@ const card = document.getElementById("card-container");
 const message = document.getElementById("message");
 
 function skapaKort(){
+    
     card.innerHTML = "";
+
+     if(saved.length === 0){
+            card.innerHTML = `<p>Du har för tillfället inga sparade dejter....</p>`;
+            card.classList.add("noDates")
+            return;
+        }
+
     for(const save of saved){
         const kortDiv = document.createElement("div");
         kortDiv.classList.add("card");
